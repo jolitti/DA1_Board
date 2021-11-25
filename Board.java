0907219a8bd6fe3c5dc43bf190f22d3f14a32b22
jolitti <marcodeltrionfo@gmail.com>
2021-11-25 17:57:08 +0100

    Added string constructor and equals operator for board

--- Board.java
+++ Board.java
@@ -1,6 +1,9 @@
+import java.lang.Math;
+import java.util.Arrays;
+
 public class Board
 {
-  private int[][] tiles; // Tiles
+  private int[][] tiles; // Array of tile values
   private int size;      // Side length
   /*public void testPrint()
   {
@@ -10,10 +13,21 @@ public class Board
   public Board(int[][] _tiles)
   {
     tiles = _tiles;
+    size = tiles.length;
   }
   public Board(String tilesSerial)
   {
-    
+    size = (int) Math.floor(Math.sqrt(tilesSerial.length()));
+    tiles = new int[size][size];
+    String[] t = tilesSerial.split(" ");
+
+    for (int j=0;j<size;j++)
+    {
+      for (int i=0;i<size;i++)
+      {
+        tiles[j][i]=Integer.parseInt(t[j*size+i]);
+      }
+    }
   }
 
   public String toString()
@@ -23,9 +37,17 @@ public class Board
     {
       for (int i=0;i<size;i++)
       {
-        result+=tiles[j][i];
+        result+=tiles[j][i] + " ";
       }
     }
     return result;
+  }
+
+  public boolean equals(Board b)
+  {
+    for (int i=0;i<size; i++) {
+      if (!Arrays.equals(tiles[i], b.tiles[i])) return false;
+    }
+    return true;
   }
 }
--- Solver.java
+++ Solver.java
@@ -2,8 +2,11 @@ class Solver
 {
 	public static void main(String[] args)
 	{
-		Board b = new Board();
+		Board b = new Board("0 1 2 3");
+		Board b2 = new Board("1 0 2 3");
 		//b.testPrint();
-
+		System.out.println(b.toString());
+		System.out.println(b.equals(b));
+		System.out.println(b.equals(b2));
 	}
 }
