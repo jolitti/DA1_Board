eea6d7a45ed528f9fdb064761e69b0890c2817cc
jolitti <marcodeltrionfo@gmail.com>
2021-11-25 17:24:17 +0100

    Sync

--- Board.java
+++ Board.java
@@ -1,8 +1,31 @@
 public class Board
 {
-
-  public void testPrint()
+  private int[][] tiles; // Tiles
+  private int size;      // Side length
+  /*public void testPrint()
   {
     System.out.println("ciao besughi");
+  }*/
+
+  public Board(int[][] _tiles)
+  {
+    tiles = _tiles;
+  }
+  public Board(String tilesSerial)
+  {
+    
+  }
+
+  public String toString()
+  {
+    String result = "";
+    for (int j=0;j<size;j++)
+    {
+      for (int i=0;i<size;i++)
+      {
+        result+=tiles[j][i];
+      }
+    }
+    return result;
   }
 }
--- Solver.java
+++ Solver.java
@@ -3,7 +3,7 @@ class Solver
 	public static void main(String[] args)
 	{
 		Board b = new Board();
-		b.testPrint();
+		//b.testPrint();
 
 	}
 }
